34a3324513b20e36f260da1be7173c843d1b924d
guychienll <guy.chien@pinkoi.com>
2023-05-12 09:38:04 +0800

    chore: step1 red

--- src/components/Picture/index.spec.js
+++ src/components/Picture/index.spec.js
@@ -3,28 +3,10 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 
-const Picture = (props) => {
-    const { src, alt } = props;
-    return (
-        <picture data-testid="picture">
-            <img data-testid="picture:image" src={src} alt={alt}></img>
-        </picture>
-    );
-};
-
 describe('picture', () => {
     it('render_as_expected', () => {
-        const props = {
-            src: 'https://via.placeholder.com/300x300.jpg',
-            alt: 'test',
-        };
-        const { queryByTestId } = render(<Picture {...props} />);
+        const { queryByTestId } = render(<picture></picture>);
 
         expect(queryByTestId('picture')).toBeInTheDocument();
-
-        const imageElem = queryByTestId('picture:image');
-        expect(imageElem).not.toBeNull();
-        expect(imageElem).toHaveAttribute('src', props.src);
-        expect(imageElem).toHaveAttribute('alt', props.alt);
     });
 });
