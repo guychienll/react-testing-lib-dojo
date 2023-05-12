869ecc28c8dc13530a293bc1b2ff37264d872d9b
guychienll <guy.chien@pinkoi.com>
2023-05-12 09:16:27 +0800

    chore: step1

--- src/App.js
+++ src/App.js
@@ -1,28 +1,10 @@
 import React from 'react';
 import './App.css';
-import Image from './components/Image';
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                <Image
-                    src="https://via.placeholder.com/300x300"
-                    lazy
-                    sizes={[
-                        {
-                            width: 300,
-                            height: 300,
-                            density: 1,
-                        },
-                        {
-                            width: 500,
-                            height: 500,
-                            density: 2,
-                        },
-                    ]}
-                />
-            </header>
+            <header className="App-header"></header>
         </div>
     );
 }
--- src/components/Image/constant.js
+++
@@ -1,7 +0,0 @@
-const PICTURE_SOURCE_TYPE = {
-    avif: 'avif',
-    webp: 'webp',
-    jpeg: 'jpeg',
-};
-
-export { PICTURE_SOURCE_TYPE };
