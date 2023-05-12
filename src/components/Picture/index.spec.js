80a2b2c6103d2fc46b45e1fff190edcf258a50d2
guychienll <guy.chien@pinkoi.com>
2023-05-12 10:01:53 +0800

    chore: step6 green

--- src/components/Picture/index.spec.js
+++ src/components/Picture/index.spec.js
@@ -49,6 +49,16 @@ describe('generate_picture_src_set', () => {
     });
 });
 
-function generatePictureSource(arg0, arg1) {
-    throw new Error('Function not implemented.');
+function generatePictureSource(src, type) {
+    let fullUrl = null;
+
+    if (src.indexOf('http') > -1 || src.indexOf('https') > -1) {
+        fullUrl = new URL(src);
+    } else {
+        fullUrl = new URL(`https://${src}`);
+    }
+
+    const urlWithoutExtension = fullUrl.href.split('.').slice(0, -1).join('.');
+
+    return `${urlWithoutExtension}.${type}`;
 }
